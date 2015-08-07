300bb496b986c428627abdb16b256755458bf3e0
Ivonei Junior <ivoneijr@gmail.com>
2015-08-07 14:53:46 -0300

    update code

--- main.rb
+++ main.rb
@@ -6,9 +6,24 @@ class Main < Sinatra::Base
   set :public_folder, File.dirname(__FILE__) + '/public'
   set :static_cache_control, [:public, :must_revalidate, :max_age => 3600]
 
-  get '/*' do
-    send_file 'resources.html'
+	get '/1' do
+    send_file 'resources1.html'
   end
 
+  get '/2' do
+    send_file 'resources2.html'
+  end
+
+  get '/3' do
+    send_file 'resources3.html'
+  end
+
+  get '/4' do
+    send_file 'resources4.html'
+  end
+
+  # get '/*' do
+  #   send_file 'resources.html'
+  # end
 
 end
